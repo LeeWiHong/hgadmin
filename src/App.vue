8663cc7660cddfa8d19c182ab8d351ceb24ae2cc
KavinKily <gzlwhong101@gmail.com>
2019-06-04 16:03:36 +0800

    重新启用antd中的layout

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,4 @@
+<!--
 <template>
   <div id="app">
     <div class="menuleft" >
@@ -15,6 +16,7 @@
 </template>
 
 <script>
+
 import Menu from '../src/components/MenuComponent'
 import MenuContent from '../src/components/ContentComponent'
 export default {
@@ -80,3 +82,67 @@ export default {
   height: 100%;
 }
 </style>
+-->
+
+<template>
+  <a-layout id="components-layout-demo-responsive">
+    <a-layout-sider
+      breakpoint="lg"
+      collapsedWidth="0"
+      @collapse="onCollapse"
+      @breakpoint="onBreakpoint"
+    >
+      <div class="logo" />
+      <a-menu theme="dark" mode="inline" :defaultSelectedKeys="['4']">
+        <a-menu-item key="1">
+          <a-icon type="user" />
+          <span class="nav-text">nav 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <a-icon type="video-camera" />
+          <span class="nav-text">nav 2</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <a-icon type="upload" />
+          <span class="nav-text">nav 3</span>
+        </a-menu-item>
+        <a-menu-item key="4">
+          <a-icon type="user" />
+          <span class="nav-text">nav 4</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header :style="{ background: '#fff', padding: 0 }" />
+      <a-layout-content :style="{ margin: '24px 16px 0' }">
+        <div :style="{ padding: '24px', background: '#fff', minHeight: '360px' }">
+          content
+        </div>
+      </a-layout-content>
+      <a-layout-footer style="textAlign: center">
+        Ant Design ©2018 Created by Ant UED
+      </a-layout-footer>
+    </a-layout>
+  </a-layout>
+</template>
+<script>
+export default {
+  methods: {
+    onCollapse(collapsed, type) {
+      console.log(collapsed, type);
+    },
+    onBreakpoint(broken) {
+      console.log(broken);
+    }
+  }
+}
+</script>
+
+<style>
+#components-layout-demo-responsive .logo {
+  height: 32px;
+  background: rgba(255,255,255,.2);
+  margin: 16px;
+}
+</style>
+
