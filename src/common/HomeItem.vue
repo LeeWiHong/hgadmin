f138443e9d7fe779c79435738dc29273fba131dd
KavinKily <gzlwhong101@gmail.com>
2019-06-05 17:58:12 +0800

    Update HomeItem.vue

--- src/common/HomeItem.vue
+++ src/common/HomeItem.vue
@@ -1,44 +1,41 @@
 <template>
-    <div>
-        <a-row type="flex" justify="center" align="buttom">
-          <a-col :span="4">
-              <a>用户总数</a>
-              <a-progress type="circle" :percent="75" :format="percent => `${percent} Days`"/>
-          </a-col>
-          <a-col :span="4">
-              <a>用户总数</a>
-              <a-progress type="circle" :percent="75" :format="percent => `${percent} Days`"/>
-          </a-col>
-          <a-col :span="4">
-              <a>用户总数</a>
-              <a-progress type="circle" :percent="75" :format="percent => `${percent} Days`"/>
-          </a-col>
-          <a-col :span="4">
-              <a>用户总数</a>
-              <a-progress type="circle" :percent="75" :format="percent => `${percent} Days`"/>
-          </a-col>
-          <a-col :span="4">
-              <a>用户总数</a>
-              <a-progress type="circle" :percent="75" :format="percent => `${percent} Days`"/>
-          </a-col>
-          <a-col :span="4">
-              <a>用户总数</a>
-              <a-progress type="circle" :percent="100" :format="percent => `${percent} Days`"/>
-          </a-col>
-        </a-row>
-    </div>
+  <a-list
+    :grid="{ gutter: 16, column: 4 }"
+    :dataSource="data"
+  >
+    <a-list-item slot="renderItem" slot-scope="item, index">
+      <a-card :title="item.title">{{item.number}}</a-card>
+    </a-list-item>
+  </a-list>
 </template>
-
 <script>
+const data = [
+  {
+    title: '用户总数',
+    number:20
+  },
+  {
+    title: '文章总数',
+    number:40
+  },
+  {
+    title: '分类总数',
+    number:50
+  },
+  {
+    title: '平台总数',
+    number:60
+  },
+  {
+    title: '建议总数',
+    number:90
+  }
+]
 export default {
-    name:'HomeItem'
+  data () {
+    return {
+      data,
+    }
+  },
 }
 </script>
-
-<style scoped>
-  div.ant-progress-circle,
-  div.ant-progress-line {
-    margin-right: 8px;
-    margin-bottom: 8px;
-  }
-</style>
