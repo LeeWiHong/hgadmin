ca603f9c19a8f9398dc24fd1cb4aa8194f953039
KavinKily <gzlwhong101@gmail.com>
2019-06-06 11:21:31 +0800

    调整发布头部信息

--- src/common/PublishHeader.vue
+++ src/common/PublishHeader.vue
@@ -1,27 +1,15 @@
 <template>
     <div>
         <div style="display:flex;flex-direction: row;">
-             <a-input placeholder="请输入标题..."></a-input>
-            <a-input placeholder="转载链接..."></a-input>
-            <a-button type="primary">发布</a-button>
-        </div>
-        
-        <a-input-group compact style="margin:10px"> 
-
-            <a-select style="z-index:300;width:80px" defaultValue="Option1-1">
-                <a-select-option v-for="(item, index) in items" :key="index" value="Option1-1">{{item}}</a-select-option>
-            </a-select>
-            <a-select  style="z-index:300;width:80px" defaultValue="Option2-2">
-                <a-select-option v-for="(item, index) in items" :key="index" value="Option2-2">{{item}}</a-select-option>
-            </a-select>
-
+             <a-input placeholder="请输入标题..." v-model="title" ></a-input>
+             <a-textarea placeholder="请输入文章简要描述..." v-model="description" :autosize="{minRow:1,maxRows:6}"/>
             <a-upload name="file" :multiple="true" action="https://www.mocky.io/v2/5cc8019d300000980a055e76" :headers="headers" @change="handleChange">
             <a-button>
                 <a-icon type="upload" /> 点击上传封面图片
             </a-button>
             </a-upload>
-            <a-textarea placeholder="请输入文章简要描述..." :autosize="{minRow:1,maxRows:6}"/>
-        </a-input-group>
+            <a-button type="primary">发布</a-button>
+        </div>
     </div>
 </template>
 
@@ -30,12 +18,19 @@ export default {
     name:'PublishHeader',
     data() {
         return {
-            items:[22,33,44,55]
+            title:'',
+            description:'',
+            body:'',
         }
+    },
+    methods: {
+        getPlatformAndCategory(){
+
+        }
+    },
+    mounted() {
+        this.getPlatformAndCategory()
     },
 }
 </script>
 
-<style scoped>
-
-</style>
