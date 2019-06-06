4cc1203fe30ccf1dd17995a1f405cb9e10a35404
KavinKily <gzlwhong101@gmail.com>
2019-06-06 17:50:19 +0800

    图片上传处理

--- src/common/PublishHeader.vue
+++ src/common/PublishHeader.vue
@@ -3,7 +3,7 @@
         <div style="display:flex;flex-direction: row;">
              <a-input placeholder="请输入标题..." v-model="title" ></a-input>
              <a-textarea placeholder="请输入文章简要描述..." v-model="description" :autosize="{minRow:1,maxRows:6}"/>
-            <a-upload name="file" :multiple="true" action="https://www.mocky.io/v2/5cc8019d300000980a055e76" :headers="headers" @change="handleChange">
+            <a-upload name="file" :customRequest="uploadImage" :multiple="true" @change="uploadArticle">
             <a-button>
                 <a-icon type="upload" /> 点击上传封面图片
             </a-button>
@@ -24,12 +24,25 @@ export default {
         }
     },
     methods: {
-        getPlatformAndCategory(){
+        uploadArticle(){
+            console.log("fffdaf")
+        },
+        uploadImage(){
+            this.$http.post('api/uploadimage',{
+                headers: {
+                    'content-type': 'multipart/form-data',
+                    'charset':'utf-8'
+                    }
 
+            }).then(response =>{
+            this.data = response.data.data
+           }).catch(error =>{
+               console.log(error)
+           })
         }
     },
     mounted() {
-        this.getPlatformAndCategory()
+        
     },
 }
 </script>
