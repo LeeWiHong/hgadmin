56d4e16121cd2d1d3347c8d1a8f407f4deb6642e
KavinKily <gzlwhong101@gmail.com>
2019-06-11 11:15:48 +0800

    平台发布接口对接

--- src/components/PlatformPublish.vue
+++ src/components/PlatformPublish.vue
@@ -1,14 +1,40 @@
 <template>
     <div>
-        <a-textarea placeholder="请输入平台名称" autosize />
-        <a-textarea placeholder="请输入平台连接" autosize />
-        <a-textarea placeholder="请输入平台描述" autosize />
-        <a-button type="primary" block>发布</a-button>
+        <a-textarea v-model:value='platname' placeholder="请输入平台名称" autosize />
+        <a-textarea v-model:value='platlink' placeholder="请输入平台连接" autosize />
+        <a-textarea v-model:value='platdescription' placeholder="请输入平台描述" autosize />
+        <a-button @click="PlatformPublishBtnClick" type="primary" block>发布</a-button>
     </div>
 </template>
 
 <script>
 export default {
-    name:'PlatformPublish'
+    name:'PlatformPublish',
+    data() {
+        return {
+            platname:'',
+            platlink:'',
+            platdescription:''
+        }
+    },
+    methods: {
+        PlatformPublishBtnClick(){
+            this.$http.get('api/addplatform',{params:{
+                platname:this.platname,
+                platlink:this.platlink,
+                platdescription:this.platdescription
+            }}).then(response=>{
+                this.$message.success(response.data.msg, 3,
+                    () => {
+                        this.$router.replace('/platform')
+                    }
+                );
+            }).catch(error=>{
+                 this.$message.error(response.data.msg, 3,
+                     () => {}
+                 );
+            })
+        }
+    },
 }
 </script>
