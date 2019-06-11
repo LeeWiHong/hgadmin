08d99ae6d57cbd528e1aace69aa0c957c4498973
KavinKily <gzlwhong101@gmail.com>
2019-06-11 14:09:46 +0800

    分类发布接口对接完毕

--- src/components/CategoryPublish.vue
+++ src/components/CategoryPublish.vue
@@ -1,15 +1,40 @@
 <template>
     <div>
-        <a-textarea placeholder="请输入分类名称" autosize />
-        <a-textarea placeholder="请输入图片地址" autosize />
-        <a-textarea placeholder="请输入分类类型" autosize />
-        <a-textarea placeholder="请输入分类描述" autosize />
-        <a-button type="primary" block="">发布</a-button>
+        <a-textarea v-model="categoryname" placeholder="请输入分类名称" autosize />
+        <a-textarea v-model="imagepath" placeholder="请输入图片地址" autosize />
+        <a-textarea v-model="description" placeholder="请输入分类描述" autosize />
+        <a-button @click="CategoryPublishBtnClick" type="primary" block="">发布</a-button>
     </div>
 </template>
 
 <script>
 export default {
-    name:'CategoryPublish'
+    name:'CategoryPublish',
+    data() {
+        return {
+            categoryname:'',
+            imagepath:'',
+            description:''
+        }
+    },
+    methods: {
+        CategoryPublishBtnClick(){
+            this.$http.get('api/addcategory',{params:{
+                categoryname:this.categoryname,
+                imagepath:this.imagepath,
+                description:this.description
+            }}).then(response=>{
+                this.$message.success(response.data.msg, 3,
+                    () => {
+                        this.$router.replace('/category')
+                    }
+                );
+            }).catch(error=>{
+                 this.$message.error(response.data.msg, 3,
+                     () => {}
+                 );
+            })
+        }
+    },
 }
 </script>
